af88bc5aa9add3c84f8785724782dbe8f41b3f46
Tomáš Brabec <tomas.brabec.98@outlook.com>
2023-12-01 15:36:11 +0100

    day 1 part 2

--- AdventOfCode2023/Calendar/Day01.cs
+++ AdventOfCode2023/Calendar/Day01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2023.Calendar;
+﻿using System.Text;
+
+namespace AdventOfCode2023.Calendar;
 
 public class Day01
 {
@@ -8,29 +10,84 @@ public class Day01
 
     private string _inputFileName;
 
+    private Dictionary<char, string> numbers = new()
+    {
+        { '1', "one" },
+        { '2', "two"},
+        { '3', "three"},
+        { '4', "four" },
+        { '5', "five" },
+        { '6', "six"},
+        { '7', "seven"},
+        { '8', "eight"},
+        { '9', "nine"}
+    };
+
     public Day01(string inputFileName)
     {
         _inputFileName = inputFileName;
         CalibrationValues = new List<int>(1000);
     }
 
-    public Day01 ProcessCalibrationValuesFromFile() 
+    public Day01 ProcessCalibrationValuesFromFile()
     {
-        foreach (string line in File.ReadLines(_inputFileName)) 
+        foreach (string line in File.ReadLines(_inputFileName))
         {
             char? firstDigit = null;
             char? lastDigit = null;
-            foreach (char c in line) 
+            StringBuilder buffer = new StringBuilder(5);
+            foreach (char c in line)
             {
-                if (!char.IsDigit(c))
-                    continue;
-
-                if (firstDigit is null)
+                if (char.IsDigit(c))
                 {
-                    firstDigit = c;
-                }
+                    buffer.Clear();
 
-                lastDigit = c;
+                    if (firstDigit is null)
+                    {
+                        firstDigit = c;
+                    }
+
+                    lastDigit = c;
+                }
+                else
+                {
+                    buffer.Append(c);
+                    string bufferdText = buffer.ToString();
+                    var matchingNumbers = numbers.Where(x => x.Value.StartsWith(bufferdText)).ToDictionary();
+                    if (matchingNumbers.Count == 0)
+                    {
+                        if (bufferdText.Length > 2)
+                        {
+                            for (int i = 1; i < bufferdText.Length; i++)
+                            {
+                                string text = bufferdText.Substring(i, bufferdText.Length - i);
+                                var matchingNumbersCount = numbers.Where(x => x.Value.StartsWith(text)).Count();
+                                if (matchingNumbersCount > 0)
+                                {
+                                    buffer.Clear();
+                                    buffer.Append(text);
+                                    break;
+                                }
+                            }
+                        }
+                        else 
+                        {
+                            buffer.Clear();
+                            buffer.Append(c);
+                        }
+                    }
+                    else if (matchingNumbers.Count == 1 && matchingNumbers.First().Value == bufferdText)
+                    {
+                        if (firstDigit is null)
+                        {
+                            firstDigit = matchingNumbers.First().Key;
+                        }
+
+                        lastDigit = matchingNumbers.First().Key;
+                        buffer.Clear();
+                        buffer.Append(c);
+                    }
+                }
             }
             CalibrationValues.Add(Convert.ToInt32($"{firstDigit}{lastDigit}"));
         }
